24e5d3817be50fcbf31a2a3c931dbdcaaa4a1b7b
RitikSaini87 <ritiksainibhai5@gmail.com>
2026-07-20 15:12:26 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Ritik Saini",
+    author_email="ritiksainibhai5@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
